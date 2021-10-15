9af50942d9e1e2883863b303881c312cb31fdc9c
evangel <evangeldaping@gmail.com>
2021-10-16 00:50:12 +0800

    Added modal to show country details.

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ const toggleBtn = document.getElementById('toggle');
 const filterBtn = document.getElementById('filter');
 const searchEl = document.getElementById('search');
 const regionFilters = filterBtn.querySelectorAll('li');
+const modal = document.getElementById('modal');
+const closeBtn = document.getElementById('close');
 
 getCountries();
 
 async function getCountries() {
     const res = await fetch('https://restcountries.com/v3.1/all');
 	const countries = await res.json();
-
+    console.log(countries);
     displayCountries(countries);
 }
 
@@ -40,9 +42,41 @@ function displayCountries(countries) {
                 </p>
             </div>
         `;
+
+        countryEl.addEventListener('click', () => {
+			modal.style.display = 'flex';
+			showCountryDetails(country);
+		});
         
 		countriesEl.appendChild(countryEl);
 	});
+}
+
+function showCountryDetails(country) {
+	const modalBody = modal.querySelector('.modal-body');
+	const modalImg = modal.querySelector('img');
+
+	modalImg.src = country.flags.svg;
+
+	modalBody.innerHTML = `
+        <h2>${country.name.common}</h2>
+        <p>
+            <strong>Population:</strong>
+            ${country.population}
+        </p>
+        <p>
+            <strong>Region:</strong>
+            ${country.region}
+        </p>
+        <p>
+            <strong>Sub Region:</strong>
+            ${country.subregion}
+        </p>
+        <p>
+            <strong>Capital:</strong>
+            ${country.capital}
+        </p>
+    `;
 }
 
 // toggle theme - dark & light
@@ -53,6 +87,11 @@ toggleBtn.addEventListener('click', () => {
 // show and hide the filters (li tags)
 filterBtn.addEventListener('click', () => {
 	filterBtn.classList.toggle('open');
+});
+
+// close the modal
+closeBtn.addEventListener('click', () => {
+	modal.style.display = 'none';
 });
 
 searchEl.addEventListener('input', e => {
